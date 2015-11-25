06cc1f8d8a803f2938e452d1a873956dc990e7d8
bigbass1997 <bigbass1997.website@gmail.com>
2015-11-25 11:37:56 -0600

    Removed unneeded/unused debug and old test code.
    (All such code was already commented out)

--- core/src/com/bigbass1997/fractaltree/world/Segment.java
+++ core/src/com/bigbass1997/fractaltree/world/Segment.java
@@ -5,17 +5,10 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.bigbass1997.fractaltree.fonts.FontManager;
 import com.bigbass1997.fractaltree.world.Object;
 
 public class Segment extends Object {
-	
-	private Stage stage;
-	private Label label;
 	
 	public float degrees;
 	public int level;
@@ -28,13 +21,8 @@ public class Segment extends Object {
 		this.pos = pos;
 		this.size = dim;
 		this.colors = colors;
-		this.degrees = degrees+90; // Makes 0 degrees point directly right as it should be. And positive is counter-clockwise.
+		this.degrees = degrees+90; // Makes 0 degrees point to the direction it should be
 		this.level = level;
-		/*
-		stage = new Stage();
-		label = new Label("", new Label.LabelStyle(FontManager.getFont("fonts/computer.ttf", 20).font, Color.ORANGE));
-		label.setPosition(pos.x, pos.y - label.getHeight());
-		stage.addActor(label);*/
 	}
 	
 	@Override
@@ -44,7 +32,6 @@ public class Segment extends Object {
 		sr.rotate(0, 0, 1, degrees);
 		sr.rect(-(size.x/2), -size.y, size.x, size.y, new Color(colors[0]), new Color(colors[1]), new Color(colors[2]), new Color(colors[3]));
 		sr.identity();
-		//stage.draw();
 	}
 	
 	@Override
@@ -57,13 +44,6 @@ public class Segment extends Object {
 		if(input.isKeyPressed(Keys.P)){ //Rotates Segment clockwise
 			degrees -= 1;
 		}
-		
-		// Debugging text
-		/*float pureDegrees = ( ( ( (degrees/360) - ((int) (degrees/360)) ) * 360 ) -90);
-		label.setText(
-				level + ".[" + (int) pos.x + ", " + (int) pos.y + "]." + degrees
-		);
-		label.setPosition(pos.x - (label.getWidth() / 2), pos.y - label.getHeight() - (MathUtils.degreesToRadians * pureDegrees * 6));*/
 	}
 	
 	public Segment copy(){
--- core/src/com/bigbass1997/fractaltree/world/Tree.java
+++ core/src/com/bigbass1997/fractaltree/world/Tree.java
@@ -29,7 +29,7 @@ public class Tree {
 	public Tree(int generations, int splits, float degreeChangeLeft, float degreeChangeRight, float initWidth, float initLength, float lengthMultiplier, float widthMultiplier){
 		segments = new ArrayList<Segment>();
 		this.generations = generations;
-		this.splits = splits;
+		this.splits = splits; // Unused at this time
 		this.degreeChangeLeft = degreeChangeLeft;
 		this.degreeChangeRight = degreeChangeRight;
 		this.initWidth = initWidth;
@@ -46,7 +46,6 @@ public class Tree {
 	}
 	
 	private void generate(int len){
-		
 		if(len > 0){
 			for(int i = 0; i < segments.size(); i++){
 				Segment seg = segments.get(i);
@@ -62,42 +61,6 @@ public class Tree {
 			}
 			generate(len - 1);
 		}
-		
-		/*float middle = (Gdx.graphics.getWidth() / 2) - 20;
-		
-		ArrayList<Segment> tmpSegs = new ArrayList<Segment>();
-		for(Segment seg : segments){
-			
-			//Create new segments here for any segments that already exist.
-			//Each segment will use 75% of length from the previous segment.
-			//A temp ArrayList of segments will have to be used to handle each new segment.
-			//Use a NEW for each loop in order to properly append the real segments list.
-			
-			Vector2 pos = new Vector2(seg.pos.x + (MathUtils.cosDeg(90) * seg.size.x), seg.pos.y + (MathUtils.sinDeg(90) * (seg.size.y * 0.75f)));
-			Vector2 size = new Vector2(5, seg.size.y * 0.75f);
-			
-			System.out.println(seg.pos + " | " + pos);
-			
-			tmpSegs.add(new Segment(pos, size, new int[]{0x000000FF,0x000000FF,0x000000FF,0x000000FF}, -45-180));
-			tmpSegs.add(new Segment(pos, size, new int[]{0x000000FF,0x000000FF,0x000000FF,0x000000FF}, 45-180));
-		}
-		for(Segment seg : tmpSegs){
-			segments.add(seg.copy());
-		}
-		tmpSegs.clear();
-		totalBranchLength--;
-		if(totalBranchLength > 0) generate();
-		
-		System.out.println(segments.size());
-		*/
-		//end
-		/*
-		segments.add(new Segment(new Vector2(middle, 20), new Vector2(5, 100), new int[]{0x000000FF,0x000000FF,0x000000FF,0x000000FF}, 0));
-		segments.add(new Segment(new Vector2(middle + (MathUtils.cosDeg(90) * 5), 20 + (MathUtils.sinDeg(90) * 100)), new Vector2(5, 75), new int[]{0x000000FF,0x000000FF,0x000000FF,0x000000FF}, -45));
-		segments.add(new Segment(new Vector2(middle + (MathUtils.cosDeg(90) * 5), 20 + (MathUtils.sinDeg(90) * 100)), new Vector2(5, 75), new int[]{0x000000FF,0x000000FF,0x000000FF,0x000000FF}, 45));
-		
-		segments.add(new Segment(new Vector2(middle, 95 + (MathUtils.sinDeg(90) * 75)), new Vector2(5, 75), new int[]{0x000000FF,0x000000FF,0x000000FF,0x000000FF}, -45));
-		*///segments.add(new Segment(new Vector2(middle, 20 + 100), new Vector2(5, 75), new int[]{0x000000FF,0x000000FF,0x000000FF,0x000000FF}, 45));
 	}
 	
 	public void render(ImmediateModeRenderer20 render, ShapeRenderer sr){
